9a384abb27d1c1687b82966d8c4c35a6d50341d6
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-17 12:32:56 -0800

    built parse tree for exercise 6.6. built stack class also to store and pop parents as we traverse parse tree to build it

--- trees.py
+++ trees.py
@@ -1,52 +1,52 @@
-# # TREE CHAPTER
-# ################### SECTION 6.4 ######################
+# TREE CHAPTER
+################### SECTION 6.4 ######################
 
-# def BinaryTree(r):
-#     return [r, [], []]
+def BinaryTree(r):
+    return [r, [], []]
 
-# def insertLeft(root,newBranch):
-#     t = root.pop(1)
-#     if len(t) > 1:
-#         root.insert(1,[newBranch,t,[]])
-#     else:
-#         root.insert(1,[newBranch, [], []])
-#     return root
+def insertLeft(root,newBranch):
+    t = root.pop(1)
+    if len(t) > 1:
+        root.insert(1,[newBranch,t,[]])
+    else:
+        root.insert(1,[newBranch, [], []])
+    return root
 
-# def insertRight(root,newBranch):
-#     t = root.pop(2)
-#     if len(t) > 1:
-#         root.insert(2,[newBranch,[],t])
-#     else:
-#         root.insert(2,[newBranch,[],[]])
-#     return root
+def insertRight(root,newBranch):
+    t = root.pop(2)
+    if len(t) > 1:
+        root.insert(2,[newBranch,[],t])
+    else:
+        root.insert(2,[newBranch,[],[]])
+    return root
 
-# def getRootVal(root):
-#     return root[0]
+def getRootVal(root):
+    return root[0]
 
-# def setRootVal(root,newVal):
-#     root[0] = newVal
+def setRootVal(root,newVal):
+    root[0] = newVal
 
-# def getLeftChild(root):
-#     return root[1]
+def getLeftChild(root):
+    return root[1]
 
-# def getRightChild(root):
-#     return root[2]
+def getRightChild(root):
+    return root[2]
 
-# # Q26 tree chapter: Write a function buildTree that returns a tree using the list of lists functions that looks like this: 
-# def buildTree():
-#     """selfcheck from tree chapter, Q26
+# Q26 tree chapter: Write a function buildTree that returns a tree using the list of lists functions that looks like this: 
+def buildTree():
+    """selfcheck from tree chapter, Q26
 
-#     >>> print(buildTree())
-#     ['a', ['b', [], ['d', [], []]], ['c', ['e', [], []], ['f', [], []]]]
-#     """
-#     x = BinaryTree('a')
-#     insertLeft(x, 'b')
-#     insertRight(getLeftChild(x), 'd')
-#     insertRight(x, 'c')
-#     r = getRightChild(x)
-#     insertLeft(r, 'e')
-#     insertRight(r, 'f')
-#     return x
+    >>> print(buildTree())
+    ['a', ['b', [], ['d', [], []]], ['c', ['e', [], []], ['f', [], []]]]
+    """
+    x = BinaryTree('a')
+    insertLeft(x, 'b')
+    insertRight(getLeftChild(x), 'd')
+    insertRight(x, 'c')
+    r = getRightChild(x)
+    insertLeft(r, 'e')
+    insertRight(r, 'f')
+    return x
 
 ################### SECTION 6.5 ######################
 
@@ -119,6 +119,48 @@ def buildTree():
     r.insert_right(BinaryTree('f'))
     r.insert_left(BinaryTree('e'))
     return x
+
+################### SECTION 6.6 ######################
+
+class Stack(object):
+    def __init__(self):
+        self.items = []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[-1]
+
+def build_parse_tree(expression):
+    expression_lst = expression.split()
+    my_stack = Stack()
+    expression_tree = BinaryTree('')
+    my_stack.push(expression_tree)
+    current_tree = expression_tree
+    for item in expression_lst:
+        if item == '(':
+            current_tree.insert_left('')
+            my_stack.push(current_tree)
+            current_tree = current_tree.get_left_child()
+        elif item not in {'+', '-', '*', '/', ')'}:
+            current_tree.set_root_val(int(item))
+            parent = my_stack.pop()
+            current_tree = parent
+        elif item in {'+', '-', '*', '/'}:
+            current_tree.set_root_val(item)
+            current_tree.insert_right('')
+            my_stack.push(current_tree)
+            current_tree = current_tree.get_right_child()
+        elif item == ')':
+            current_tree = my_stack.pop()
+        else:
+            raise ValueError
+
+    return expression_tree
+
+pt = build_parse_tree("( ( 10 + 5 ) * 3 )")
+print pt
 
 if __name__ == "__main__":
     import doctest
