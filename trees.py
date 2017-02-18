d57dfd486237fe1f26b662677af1ea6648e3536a
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-02-17 16:30:51 -0800

    tree node and binary search tree class from chapter 6.13

--- trees.py
+++ trees.py
@@ -180,19 +180,80 @@ def preorder(tree):
         preorder(tree.left_child)
         preorder(tree.right_child)
 
+# postorder traversal
 def postorder(tree):
     if tree is not None:
         postorder(tree.left_child)
         postorder(tree.right_child)
         print(tree)
 
+# inorder traversal
 def inorder(tree):
     if tree is not None:
         inorder(tree.left_child)
         print tree
         inorder(tree.right_child)
 
-inorder(pt)
+################### SECTION 6.9 ######################
+# min heap
+
+################### SECTION 6.13 ######################
+# binary search tree
+
+class BinarySearchTree(object):
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+class TreeNode(object):
+
+    def __init__(self, kay, val, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = val
+        self.left_child = left
+        self.right_child = right
+        self.parent = parent
+
+    def has_left_child(self):
+        return self.left_child
+
+    def has_right_child(self):
+        return self.right_child
+
+    def is_left_child(self):
+        return self.parent and self.parent.left_child == self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.right_child or self.left_child)
+
+    def has_any_children(self):
+        return self.right_child or self.left_child
+
+    def replace_node_data(self, key, value, lc, rc):
+        self.key = key
+        self.payload = value
+        self.left_child = lc
+        self.right_child = rc
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self
 
 if __name__ == "__main__":
     import doctest
